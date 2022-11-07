463e78c59c2c13aeab51c466b130ee82cbc55dcd
davidsnom <117677786+davidsnom@users.noreply.github.com>
2022-11-07 20:09:37 +0100

    Add files via upload

---
+++ Ejercicio04.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio04;
+
+import bpc.daw.mario.*;
+/**
+ *
+ * @author usuario-maÃ±ana
+ */
+public class Ejercicio04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    String str1 ="hola";
+Mario m1 = null;
+Luigi l1 = null;
+Seta s1 = null;
+Cañon c1 = null;
+Planta p1 =null;
+m1 = new Mario (540,500);
+double distancia = m1.getDistanciaOrigen();
+System.out.println("Mario esta en " + distancia);
+l1 = new Luigi(500,500);
+double distanciaa = l1.getDistanciaOrigen();
+System.out.println("luigi esta en " + distanciaa);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+    
+}
