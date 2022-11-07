bb3dfe8686d5776479a8e90c9f8507fac4373e10
davidsnom <117677786+davidsnom@users.noreply.github.com>
2022-11-07 21:34:35 +0100

    Add files via upload

---
+++ 02_EjerciciosT2/Ejercicio06.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio06;
+import bpc.daw.mario.*;
+import java.util.Scanner;
+
+/**
+ *
+ * @author usuario-mañana
+ */
+public class Ejercicio06 {
+
+    
+
+    public static void main(String[] args) {
+        System.out.println("Dime las coordenadas");
+double x;
+double y;
+Mario m1 =null;
+int coordenadax=new Scanner (System.in).nextInt();
+ int coordenaday=new Scanner(System.in).nextInt();
+     m1= new Mario(coordenadax,coordenaday);0
+             0
+     
+    }
+    
+}
